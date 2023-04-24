9e871f2824165578dc8095da154852cb2265d4ee
Temith <akanbitilayo17@gmail.com>
2023-04-24 05:39:19 -0700

    file added

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: "print all single digit number of base 10"
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}

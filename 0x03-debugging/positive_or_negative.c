46dc8b07adb3b625602877dafd89b1028ed8517b
Temith <akanbitilayo17@gmail.com>
2023-04-27 15:12:35 -0700

    file edit

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* main - Entry point
+* Descriotion: "check if the integer is positive or negative"
+* Return: Always 0 (Success)
+*/
+
+void positive_or_negative(int i);
+{
+	if (i < 10)
+	{
+		printf("%d is negative", i);
+	}
+	else
+	{
+		printf("%d is positive", i);
+	}
+	printf("\n");
+
+	return (0);
+}

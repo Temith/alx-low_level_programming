9f99451b371cb2ce74e26c93695ff1f6ec51ee3f
Temith <akanbitilayo17@gmail.com>
2023-05-03 22:58:09 -0700

    file adjust

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,11 +7,22 @@
 void puts2(char *str)
 {
 	int i;
+	int len = 0;
+	int t = 0;
+	char *y = str;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*y != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		y++;
+		len++;
+	}
+	t = len - 1;
+	for (i = 0; i <= t; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }

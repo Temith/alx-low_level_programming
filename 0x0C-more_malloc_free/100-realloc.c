3e5d10b0787e32b827a3b5b484f9573a2e728ed5
Temith <akanbitilayo17@gmail.com>
2023-05-18 17:37:46 -0700

    file adjusted

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,54 +1,40 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 /**
- * simple_print_buffer - prints in hexa
- * @buffer: the aggress of memory to print
- * @size: the size of memory to print
+ * *_realloc - reallocate a memory block
+ * @ptr: pointer to memory
+ * @old_size: size in bytes
+ * @new_size: size in bytes of new memory
  *
- * Return: nothing
+ * Return: NULL if new_size is 0, ptr if is not NULL
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *p;
 	unsigned int i;
 
-	i = 0;
-
-	while (i < size)
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
 	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
+		free(ptr);
+		return (NULL);
 	}
-	printf("\n");
+	if (ptr == NULL)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+	}
+	return (p);
+	if (new_size > old_size)
+	{
+		p = malloc(new_size);
+		if (p == NULL)
+			return (NULL);
+		for (i = 0; i < old_size && i < new_size; i++)
+			*((char *)p + i) = *((char *)ptr + i);
+		free(ptr);
+	}
+	return (p);
 }
 
-/**
- * main- check the code
- *
- * Return: always 0
- */
-int main(void)
-{
-	char *p;
-	int i;
-
-	p = malloc(sizeof(char) * 10);
-	p = _realloc(p, sizeof(char) * 10, sizeof(char) * 98);
-	i = 0;
-
-	while (i < 98)
-	{
-		p[i++] = 98;
-	}
-	simple_print_buffer(p, 98);
-	free(p);
-	return (0);
-}
